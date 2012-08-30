c8eb06e3e8679658f08e45d0a7b4eb7561ea8b87
Andrew Hart <ahart@apache.org>
2012-08-30 21:07:14 +0000

    update look and feel for the rcmet webapp header and footer
    git-svn-id: https://svn.apache.org/repos/asf/oodt/branches/rmcesjpl@1473976 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 12cb9bbecc52bb47f2569711b53408a5adaaaa5e

--- projects/rcmes/trunk/rcmet/src/main/webapp/views/common/footer.php
+++ projects/rcmes/trunk/rcmet/src/main/webapp/views/common/footer.php
@@ -1,14 +1,16 @@
 <?php
 ?>
-<footer>
-<div class="bal_footer">
-Regional Climate Model Evaluation Toolkit 
-|&nbsp;Jet Propulsion Laboratory, California Institute of Technology 
-|&nbsp;OODT Balance v.<?php echo BALANCE_VERSION ?>
-</div>
+        </div><!-- end '.page' -->
+    </div>
+    <footer>
+        <div id="bal_footer">
+        Regional Climate Model Evaluation Toolkit 
+        |&nbsp;Jet Propulsion Laboratory, California Institute of Technology 
+        |&nbsp;OODT Balance v.<?php echo BALANCE_VERSION ?>
+        </div>
 
-<!-- html closing statements -->
-</footer>
+        <!-- html closing statements -->
+    </footer>
 </div> <!-- end #container -->
 <script type="text/javascript" src="<?php echo SITE_ROOT?>/static/bootstrap/js/bootstrap-min.js"/>
 </body>
