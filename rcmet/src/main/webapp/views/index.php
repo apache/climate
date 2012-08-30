d77f14c874fcd9f93cf0fea31295d2ef4b941fe0
Andrew Hart <ahart@apache.org>
2012-08-30 21:08:20 +0000

    update look and feel for rcmet webapp home page
    git-svn-id: https://svn.apache.org/repos/asf/oodt/branches/rmcesjpl/projects/rcmes/trunk@1473979 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 155fb74b257ec443683e0760dad7b98024de1aca

--- rcmet/src/main/webapp/views/index.php
+++ rcmet/src/main/webapp/views/index.php
@@ -1,30 +1,42 @@
-<div class="span-17">
-	<div class="box span-7 colborder">
-		<h3 class="big"><a href="http://wrm.jpl.nasa.gov/">Browse the Database</a></h3>
-		The Regional Climate Model Evaluation Database contains
-		decades of observational datasets from NASA and other
-		organizations. 
-	</div>
-	<div class="box span-7 last">
-		<h3 class="big"><a href="<?php echo SITE_ROOT?>/wizard/init">Begin an Evaluation</a></h3>
-		Combine local model data with observations from the database
-		to perform analysis and generate visualizations.
-	</div>
-	<hr/>
+<div class="section">
+    <div class="row">
+	    <div class="box span6">
+	        <div class="box-content">
+		        <h3><a href="http://rcmed.jpl.nasa.gov/">Browse the Database</a></h3>
+		        The Regional Climate Model Evaluation Database contains
+		        decades of observational datasets from NASA and other
+		        organizations.
+		    </div> 
+	    </div>
+	    <div class="box span6">
+	        <div class="box-content">
+		        <h3><a href="<?php echo SITE_ROOT?>/wizard/init">Begin an Evaluation</a></h3>
+		        Combine local model data with observations from the database
+		        to perform analysis and generate visualizations.
+	        </div>
+	    </div>
+	    
+    </div>
+    <div class="row">
+        <div class="box span12">
+            <div class="box-content">
+	            
+	            <img src="<?php echo SITE_ROOT?>/static/img/rcmes-logo.png" id="rcmes-logo"/>
+	            <h5>Background Information</h5>
+	            <p>
+	            The Regional Climate Model Evaluation Toolkit is comprised of 
+	            scripts and interfaces that make it easy to interact with the observational
+	            data provided by the Regional Climate Model Evaluation Database for the 
+	            purpose of conducting model-to-data intercomparison and validation.
+	            </p>
+	            <h5>Getting Help</h5>
+	            <p>
+	            The Regional Climate Model Evaluation System is actively being developed
+	            at the NASA Jet Propulsion Laboratory (JPL), in collaboration with the UCLA Joint
+	            Institute for Regional Earth System Science and Engineering (JIFRESSE). The 
+	            latest information is always available on the official
+	            <a href="http://rcmes.jpl.nasa.gov">RCMES Website</a>.
+            </div>
+        </div>
+    </div>
 </div>
-<div class="span-7 last">
-	<h5>Background Information</h5>
-	<p>
-	The Regional Climate Model Evaluation Toolkit is comprised of 
-	scripts and interfaces that make it easy to interact with the observational
-	data provided by the Regional Climate Model Evaluation Database for the 
-	purpose of conducting model-to-data intercomparison and validation.
-	</p>
-	<h5>Getting Help</h5>
-	<p>
-	The Regional Climate Model Evaluation System is actively being developed
-	at the NASA Jet Propulsion Laboratory (JPL), in collaboration with the UCLA Joint
-	Institute for Regional Earth System Science and Engineering (JIFRESSE). The 
-	latest information is always available on the <a href="#">RCMES Website</a>.
-</div>
-<br class="clear"/>
