ab0ffa93238cea4c807129c728c4bbd8714e1566
cgoodale <cgoodale@unknown>
2012-09-18 14:52:35 +0000

    Additional classes created by Paul Ramirez and I.  Still a work in progress.
    git-svn-id: https://svn.apache.org/repos/asf/oodt/branches/rmcesjpl/projects/rcmes/trunk@1474154 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3ba32b6efe96d13eaeb83075b14f8abcc3fc8d7b

--- rcmet/src/main/python/rcmes/classes.py
+++ rcmet/src/main/python/rcmes/classes.py
@@ -1,14 +1,34 @@
 import os
+from datetime import datetime as datetime
 
 import storage.files as files
+import toolkit.process as process
+
+class BoundingBox(object):
+    
+    def __init__(self, latMin, lonMin, latMax, lonMax):
+        self.latMin = latMin
+        self.lonMin = lonMin
+        self.latMax = latMax
+        self.lonMin = lonMax
+        
+        
+class SubRegion(object):
+    
+    def __init__(self, name, bbox):
+        self.name = name
+        self.bbox = bbox
 
 class Settings(object):
     
-    def __init__(self, config):
-        
-        self.workDir = os.path.abspath(config['workdir'])
-        self.cacheDir = os.path.abspath(config['cachedir'])
-        
+    def __init__(self, workDir, cacheDir, startDate, endDate, spatialGrid, gridLonStep, gridLatStep):
+        self.workDir = os.path.abspath(workDir)
+        self.cacheDir = os.path.abspath(cacheDir)
+        self.startDate = datetime.strptime(startDate, '%Y%m%d')
+        self.endDate = datetime.strptime(endDate, '%Y%m%d')
+        self.spatialGrid = spatialGrid
+        self.gridLonStep = float(gridLonStep)
+        self.gridLatStep = float(gridLatStep)
         """
         TODO:  These will be split apart into Object Attributes
         self.latMin=config['latmin']
@@ -36,18 +56,20 @@ class Settings(object):
     
 class Model(object):
     
-    def __init__(self, config):
-        self.filename = config['filename']
-        self.latVariable = config['latvariable']
-        self.lonVariable = config['lonvariable']
-        self.timeVariable = config['timevariable']
-        self.timeStep = config['timestep']
-        self.varName = config['varname']
-        
+    def __init__(self, filename, latVariable, lonVariable, timeVariable, timeStep, varName, precipFlag):
+        self.filename = filename
+        self.latVariable = latVariable
+        self.lonVariable = lonVariable
+        self.timeVariable = timeVariable
+        self.timeStep = timeStep
+        self.varName = varName
+        self.times = process.getModelTimes(self.filename, self.timeVariable)
+        self.minTime = min(self.times)
+        self.maxTime = max(self.times)
         self.setLatitudeRange()
         self.setLongitudeRange()
     
-        if config['precipflag'] == 'True':
+        if precipFlag == 'True':
             self.precipFlag = True
         else:
             self.precipFlag = False
@@ -58,3 +80,24 @@ class Model(object):
     
     def setLongitudeRange(self):
         self.lonMin, self.lonMax = files.getVariableRange(self.filename, self.lonVariable)
+
+
+class Parameter(object):
+    def __init__(self, param_id, shortName=None, description=None, endDate=None ):
+        pass
+
+class RCMED(object):
+    def __init__(self):
+        pass
+    
+    def retriveData(self, parameter, dataBounds, timeStep=None):
+        # Returns the data
+
+        pass
+
+class RCMEDDataset(object):
+    
+    def __init__(self, obsDatasetId, obsParamId, obsTimeStep):
+        self.datasetId = obsDatasetId
+        self.parameterId = obsParamId
+        self.timeStep = obsTimeStep
