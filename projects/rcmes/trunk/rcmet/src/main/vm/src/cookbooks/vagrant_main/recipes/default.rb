dbdda0a6e991871f12ad7c2629d2358f207ba9ca
Andrew Hart <ahart@apache.org>
2012-10-10 00:55:02 +0000

    enable the rcmet webapp vhost at startup
    git-svn-id: https://svn.apache.org/repos/asf/oodt/branches/rmcesjpl@1474266 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 2b93300b3fef4c69742aa314a7e7e7b96aafb2ae

--- projects/rcmes/trunk/rcmet/src/main/vm/src/cookbooks/vagrant_main/recipes/default.rb
+++ projects/rcmes/trunk/rcmet/src/main/vm/src/cookbooks/vagrant_main/recipes/default.rb
@@ -5,3 +5,8 @@ execute "disable-default-site" do
   command "sudo a2dissite default"
   notifies :reload, resources(:service => "apache2"), :delayed
 end
+
+web_app "rcmet" do
+  template "rcmet.conf.erb"
+  notifies :reload, resources(:service => "apache2"), :delayed
+end
