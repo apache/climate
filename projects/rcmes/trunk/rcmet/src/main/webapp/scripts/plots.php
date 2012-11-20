efe2d5f638c4fe088535bd86be97c8dbb3a3d5c3
Andrew Hart <ahart@apache.org>
2012-11-20 16:20:28 +0000

    make the use of curl optional (default false) and controlled via configuration
    git-svn-id: https://svn.apache.org/repos/asf/oodt/branches/rmcesjpl@1474523 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3d83a99367e41404b426699f56ded030f1a2f915

--- projects/rcmes/trunk/rcmet/src/main/webapp/scripts/plots.php
+++ projects/rcmes/trunk/rcmet/src/main/webapp/scripts/plots.php
@@ -6,14 +6,25 @@ switch (strtoupper($_REQUEST['action'])) {
 		$url        = App::Get()->settings['rcmet_service_url_base'] . '/rcmes/run/';
 		$postFields = http_build_query($_REQUEST);
 		
-		$ch = curl_init();
-		curl_setopt($ch,CURLOPT_URL,$url);
-		curl_setopt($ch,CURLOPT_POST, 1);
-		curl_setopt($ch,CURLOPT_POSTFIELDS,$postFields);
-		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-		#curl_setopt($ch,CURLOPT_HEADER,1);
-		echo curl_exec($ch);
-		curl_close($ch);
+		if (isset(App::Get()->settings['rcmet_service_use_curl']) 
+		   && App::Get()->settings['rcmet_service_use_curl'] == true) {
+		   $ch = curl_init();
+		   curl_setopt($ch,CURLOPT_URL,$url);
+		   curl_setopt($ch,CURLOPT_POST, 1);
+		   curl_setopt($ch,CURLOPT_POSTFIELDS,$postFields);
+		   curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+		   $response = curl_exec($ch);
+		   curl_close($ch);
+		} else {
+		  $params = array('http' => array(
+		     'method' => 'POST',
+		     'content' => $postFields));
+		  $ctx = stream_context_create($params);
+		  $fp = @fopen($url, 'rb', false, $ctx);
+		  $response = @stream_get_contents($fp);
+		}
+
+		echo $response;
 		break;
 	default:
 		echo "{}";
