431d0485de1a687c68ef681db1a1b706ddf4b900
Andrew Hart <ahart@apache.org>
2012-11-20 00:43:33 +0000

    update main recipe to create rcmet user and establish virtualenv for importing python code
    git-svn-id: https://svn.apache.org/repos/asf/oodt/branches/rmcesjpl/projects/rcmes/trunk@1474519 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c294de8ac7c58b228d37bb60c2e508ca6b10c355

--- rcmet/src/main/vm/src/cookbooks/vagrant_main/recipes/default.rb
+++ rcmet/src/main/vm/src/cookbooks/vagrant_main/recipes/default.rb
@@ -1,5 +1,34 @@
-require_recipe "apt"
-require_recipe "apache2"
+# APT Package Management --------------------------------------------
+# Install the APT package management tool, and then install any
+# packages that we will need to have to complete the buildout.
+#
+include_recipe "apt"
+include_recipe "build-essential"
+gem_package "ruby-shadow" do
+  action :install
+end
+
+# User Creation -----------------------------------------------------
+# Create a user account for the 'rcmet' user
+#
+user "rcmet" do
+  comment "RCMET user"
+  uid 1001
+  #gid "users"
+  home "/usr/local/rcmet"
+  shell "/bin/bash"
+  #password "1bob1bob"
+end
+
+directory "/usr/local/rcmet" do
+  mode "0777"
+end
+
+# Apache Web Server Configuration -----------------------------------
+# Install the Apache2 HTTPD Web Server, and configure a virtual host
+# for the RCMET web application. 
+#
+include_recipe "apache2"
 
 execute "disable-default-site" do
   command "sudo a2dissite default"
@@ -10,3 +39,18 @@ web_app "rcmet" do
   template "rcmet.conf.erb"
   notifies :reload, resources(:service => "apache2"), :delayed
 end
+
+# Python Environment ------------------------------------------------
+# Install Python and package management tools 'pip' and 'virtualenv'.
+# Then create a virtual environment in  the 'rcmet' user's home 
+# directory, into which all RCMET python code and dependencies can be 
+# installed.
+#
+include_recipe "python"
+
+python_virtualenv "/usr/local/rcmet/env" do
+  owner "rcmet"
+  group "rcmet"
+  action :create
+end
+
