e1b55d2f91a805152fd0f62080582a7538f1cfd0
mjjoyce <mjjoyce@unknown>
2013-01-17 17:12:59 +0000

    virtualenv install added
    git-svn-id: https://svn.apache.org/repos/asf/oodt/branches/rmcesjpl/projects/rcmes/trunk@1474777 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 02e914e9d057524508cd1281e1239b3fe97d510f

--- easy-rcmet/install.sh
+++ easy-rcmet/install.sh
@@ -24,6 +24,13 @@ header()
     echo "---------------------------------------------------------------------------" 
     echo
 }
+
+subheader()
+{
+    echo
+    echo ">" $1
+    echo
+}
     
 ### Argument Parsing
 while getopts "heq" FLAG
@@ -53,7 +60,7 @@ echo "                         Welcome to Easy RCMET"
 echo "---------------------------------------------------------------------------" 
 echo
 
-if (($WITH_INTERACT == 1))
+if [ $WITH_INTERACT == 1 ]
 then
 cat << ENDINTRO
 The following packages will be installed:
@@ -63,21 +70,77 @@ It is highly recommended that you allow this script to install and configure a
 virtualenv environment for you. If you didn't pass the -e flag when running
 this script, you're fine! Otherwise, know what you're doing! Parts of this
 script will pollute your global Python install if you don't run within a
-virtualenv environment.
+virtualenv environment. If you already have a virtualenv environment into which
+you want installs to go, then make sure you don't pass -e!
 
 ENDINTRO
 
 read -p "Press [ENTER] to being installation..."
 fi
 
+### virtualenv install (if required)
+if [$WITH_VIRTUAL_ENV == 1]
+then
+    if ! command -v pip >/devl/null
+    then
+        subheader "Installing Pip (and distribute just in case)"
+        subheader "Distribute"
+        curl http://python-distribute.org/distribute_setup.py | python
+
+        subheader "Pip"
+        curl -O http://pypi.python.org/packages/source/p/pip/pip-1.2.1.tar.gz
+        tar xzf pip-1.2.1.tar.gz
+        cd pip-1.2.1/
+        python setup.py install
+    fi
+
+    subheader "Installing virtualenv..."
+    pip install virtualenv
+    subheader "Installing virtualenvwrapper..."
+    pip install virtualenvwrapper
+
+    # Need to setup environment for virtualenv
+    export WORKON_HOME=$HOME/.virtualenvs
+    source /usr/local/bin/virtualenvwrapper.sh
+
+    # Create a new environment for RCMES work
+    mkvirtualenv rcmes
+    workon rcmes
+fi
+
 ### Build
 header "Begin Buildout"
 python bootstrap.py -d
-./bin/buildout
+./bin/buildout -vvvvv
 
 ### Cleanup
-header "Begin Cleanup"
+header "Begin Clean-up"
 rm -rf bin/ develop-eggs/ eggs/ parts/
 
 ### Outro
 header "Easy RCMET installation complete."
+
+cat << OUTRO
+If this installed virtualenv for you, make sure you set the following values in
+your shell's RC file.
+
+    export WORKON_HOME=$HOME/.virtualenvs
+    source /usr/local/bin/virtualenvwrapper.sh
+
+Similarly, the default virtualenv environment that this script creates for you
+to use during RCMES work is called "rcmes". Whenever you want to work on RCMES
+tasks, be sure to run
+
+    workon rcmes
+
+This will activate the RCMES environment. When you're done, run
+
+    deactivate rcmes
+
+If you would like to read more about how to use virtualenv and 
+virtualenvwrapper, please look to the following websites for documentation.
+
+    http://www.virtualenv.org/en/latest/
+    http://virtualenvwrapper.readthedocs.org/en/latest/
+
+OUTRO
