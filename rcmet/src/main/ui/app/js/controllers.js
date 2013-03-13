3e874dc936144287d8156b204e79daf726dc8da8
mjjoyce <mjjoyce@unknown>
2013-03-13 02:34:51 +0000

    Added framework for dataset parameter tweaking area.
    Cleaned out old multiview code that is no longer needed.
    Added fields for parameter adjustments
    Added some control buttons and skeleton code for their actions
    Evaluate and update buttons should properly deactivate when there is a
        lack of datasets
    Tests for the controllers are present to test:
        Proper init of ParametSelectCtrl
        Proper functionality of shouldDisableControls()
    Adjusted the padding on the map so stuff doesn't stick to the bottom
    
    git-svn-id: https://svn.apache.org/repos/asf/oodt/branches/rmcesjpl/projects/rcmes/trunk@1475150 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: d25280a59a52b07992349026048ba81dc4cf3e53

--- rcmet/src/main/ui/app/js/controllers.js
+++ rcmet/src/main/ui/app/js/controllers.js
@@ -1,9 +1,23 @@
 'use strict';
 
-// Controller for Model Selection partial
-function ModelSelectCtrl($scope) {
+// Controller for the world map
+function WorldMapCtrl($scope) {
 }
 
-//Controller for the world map
-function WorldMapCtrl($scope) {
+// Controller for dataset parameter selection/modification
+function ParameterSelectCtrl($scope) {
+	$scope.numberOfDatasets = 0;
+
+	$scope.shouldDisableControls = function() {
+		return ($scope.numberOfDatasets < 2);
+	}
+
+	$scope.runEvaluation = function() {
+	}
+
+	$scope.addDataset = function() {
+	}
+
+	$scope.updateParameters = function() {
+	}
 }
