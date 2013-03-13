53eb94eca173006b997c5008033a31d49002dc66
mjjoyce <mjjoyce@unknown>
2013-03-13 02:34:43 +0000

    Begin work on moving to different views
        Input a rough section below the map for  model load. This will
          probably be moved at some point I would guess...
    
        Skeleton code for Model Load added.
    
        Fixed issue with multiple module declarations breaking the display.
    
    git-svn-id: https://svn.apache.org/repos/asf/oodt/branches/rmcesjpl/projects/rcmes/trunk/rcmet/src/main/ui@1475143 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 0dee6a9d401c692dd9b6a7dc731c7010a8d5ceaa

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,8 @@
 'use strict';
 
-
-// Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+angular.module('rcmes', []).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
+  $routeProvider.
+      when('/modelSelect', {templateUrl: 'partials/modelSelect.html', controller: ModelSelectCtrl}).
+      otherwise({redirectTo: '/modelSelect'});
+}]);
