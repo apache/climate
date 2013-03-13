6f484987187347b56b80e33511a32463c643c72f
mjjoyce <mjjoyce@unknown>
2013-03-13 02:34:40 +0000

    Got map added to app
    git-svn-id: https://svn.apache.org/repos/asf/oodt/branches/rmcesjpl/projects/rcmes/trunk/rcmet/src/main/ui@1475141 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 5760277614a229efeb64d42fb186805ebd9838c6

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,11 +1,20 @@
 'use strict';
 
-/* Directives */
-
-
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
+angular.module('RCMES', []).
+    directive('sap', function() {
+    return {
+        restrict: 'E',
+        replace: true,
+        template: '<div></div>',
+        link: function(scope, element, attrs) {
+            var map = L.map(attrs.id, {
+                center: [40, -86],
+                zoom: 2
+            });
+            //create a CloudMade tile layer and add it to the map
+            L.tileLayer('http://{s}.tile.cloudmade.com/57cbb6ca8cac418dbb1a402586df4528/997/256/{z}/{x}/{y}.png', {
+                maxZoom: 18
+            }).addTo(map);
+        }
     };
-  }]);
+});
