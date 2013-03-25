fddd1508415137fd1b49c93f6b3dc1ffd3a2a34e
mjjoyce <mjjoyce@unknown>
2013-03-25 17:11:52 +0000

    Added Directive for handling bootstrapModal.
    	Minor changes to the formatting of the existing directive so the
    	declarations can be chained.
    
    	Added directive for processing bootstrapModal
    
    git-svn-id: https://svn.apache.org/repos/asf/oodt/branches/rmcesjpl/projects/rcmes/trunk@1475237 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3097f0b0131d38d7e77d3324582767faca2667e6

--- rcmet/src/main/ui/app/js/directives.js
+++ rcmet/src/main/ui/app/js/directives.js
@@ -1,7 +1,8 @@
 'use strict';
 
+// Directive for dealing with the Leaflet map
 angular.module('rcmes', []).
-    directive('sap', function() {
+directive('sap', function() {
     return {
         restrict: 'E',
         replace: true,
@@ -17,4 +18,78 @@ angular.module('rcmes', []).
             }).addTo(map);
         }
     };
+}).
+// Directive for inserting bootstrap modals
+directive('bootstrapModal', function($defer) {
+	var link = function(scope, elem, attrs) {
+		var escapeEvent;
+		var openModal;
+		var closeModal;
+
+		escapeEvent = function(e) {
+			if (e.which == 27)
+				closeModal();
+		}
+
+		openModal = function(event, hasBackground, hasEscapeExit) {
+			// Grab the current modal tag based on the modalId attribute in the bootstrapModal tag
+			var modal = $('#' + attrs.modalID);
+
+			if (hasBackground) {
+				// Check if there is a modalBackground tag and add it if it doesn't exist
+				if (!document.getElementById('modalBackground')) {
+					$('body').append('<div id="modalBackground" class="modalBackground"></div>');
+				}
+
+				// Show the modal background and bind a click event to the function for closing the modal
+				$('#modalBackground').
+					css({display: 'block'}).
+					bind('click', closeModal);
+			}
+
+			// Pressing escape should close the modal
+			if (hasEscapeExit) {
+				$('body').bind('keyup', escapeEvent);
+			}
+
+			// Make all the modal's children of class "close" call the appropriate function for closing!
+			$('.close', modal).bind('click', closeModal);
+
+			// Display the modal
+			modal.css({display: 'block'});
+		}
+
+		closeModal = function(event) {
+			// Clean up the modalBackground by removing the click event and hiding it.
+			$('#modalBackground').
+				unbind('click', closeModal).
+				css({diplay: 'none'});
+
+			// Remove the event for closing the body from the body
+			$('body').
+				unbind('keyup', escapeEvent)
+
+			// Hide the modal
+			$('#' + attrs.modalId).css({display: 'hidden'});
+		}
+
+		// We need to bind the close and open modal events so outside elements can trigger the modal.
+		// This has to wait until the template has been fully inserted, so just wait a bit of time
+		// before we set them. I'm sure there's a better way of handling this...
+		$defer(function() {
+			$('#' + attrs.modalId).
+				bind('modalOpen', openModal).
+				bind('modalClose', closeModal);
+		}, 100);
+	};
+
+	return {
+		link: link,
+		restric: 'E',
+		scope: {
+			modalId: 'attribute'
+		},
+		template: '<div id="{{modalId}}" class="modal hide"><div ng-transclude></div></div>',
+		transclude: true
+	};
 });
