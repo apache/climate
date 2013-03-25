db662d657cde67e317953bee95133a7dbf22e232
mjjoyce <mjjoyce@unknown>
2013-03-25 17:12:05 +0000

    Added skeleton for observation selection modal.
    git-svn-id: https://svn.apache.org/repos/asf/oodt/branches/rmcesjpl/projects/rcmes/trunk/rcmet/src/main/ui@1475247 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c7c47f6975cd5e88d8ce979b5bc0579503590f1c

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -24,4 +24,17 @@ function ParameterSelectCtrl($scope) {
 
 // Controller for observation selection in modal
 function ObservationSelectCtrl($scope) {
+	$scope.params = ["Please select a file above"];
+	$scope.lats = ["Please select a file above"];
+	$scope.lons = ["Please select a file above"];
+	$scope.times = ["Please select a file above"];
+
+	$scope.uploadLocalFile = function() {
+		// Need to make a request to the bottle services
+		// Then upload the local bindings
+	};
+
+	$scope.addDataSet = function() {
+		// Need to add the dataset information to the correct model for the main div
+	}
 }
