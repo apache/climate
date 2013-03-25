88cb60a36142f5d29571003761e0905fdcf545b4
mjjoyce <mjjoyce@unknown>
2013-03-25 17:11:55 +0000

    Updated directives.js for proper modal handling.
    Minor indentation adjustments on Leaflet directive
    
    "bootstrapModal" directive now uses "timeout" function instead of "defer"
    
    Selecting the modal with jQuery now references the correct attribute
    
    Fixed the incorrect class names that were being used for modal backdrop
    -- "modalBackground" switched out for "modal-backdrop"
    -- Changes were made to both the insertion and removal of the backdrop in
    	openModal and closeModal respectively
    
    Minor syntax issues:
    -- Function declarations weren't closed with a colon
    -- Removal of modal-backdrop had incorrect css attribute 'diplay'
    -- Modal hiding was broken because of css attribute 'display: hidden'
    	instead of 'display: none'
    
    "bootstrapModal" return changes:
    -- Now has replace value set so the substitution is performed correctly
    -- Scope value now references modalId properly from the parent scope
    	with '@' instead of the deprecated syntax used previously.
    
    "bootstrapModalOpen" directive changes:
    -- The trigger call now properly passes the variables hasBackground and
    	hasEscapeExit
    -- Minor beautifications
    
    git-svn-id: https://svn.apache.org/repos/asf/oodt/branches/rmcesjpl@1475240 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: d84292c9be449657fd397148534a4daeb27f4930

--- projects/rcmes/trunk/rcmet/src/main/ui/app/js/directives.js
+++ projects/rcmes/trunk/rcmet/src/main/ui/app/js/directives.js
@@ -3,24 +3,24 @@
 // Directive for dealing with the Leaflet map
 angular.module('rcmes', []).
 directive('sap', function() {
-    return {
-        restrict: 'E',
-        replace: true,
-        template: '<div></div>',
-        link: function(scope, element, attrs) {
-            var map = L.map(attrs.id, {
-                center: [40, -86],
-                zoom: 2
-            });
-            //create a CloudMade tile layer and add it to the map
-            L.tileLayer('http://{s}.tile.cloudmade.com/57cbb6ca8cac418dbb1a402586df4528/997/256/{z}/{x}/{y}.png', {
-                maxZoom: 4, minZoom: 2
-            }).addTo(map);
-        }
-    };
+	return {
+		restrict: 'E',
+		replace: true,
+		template: '<div></div>',
+		link: function(scope, element, attrs) {
+			var map = L.map(attrs.id, {
+				center: [40, -86],
+				zoom: 2
+			});
+			//create a CloudMade tile layer and add it to the map
+			L.tileLayer('http://{s}.tile.cloudmade.com/57cbb6ca8cac418dbb1a402586df4528/997/256/{z}/{x}/{y}.png', {
+				maxZoom: 4, minZoom: 2
+			}).addTo(map);
+		}
+	};
 }).
 // Directive for inserting bootstrap modals
-directive('bootstrapModal', function($defer) {
+directive('bootstrapModal', function($timeout) {
 	var link = function(scope, elem, attrs) {
 		var escapeEvent;
 		var openModal;
@@ -33,16 +33,16 @@ directive('bootstrapModal', function($defer) {
 
 		openModal = function(event, hasBackground, hasEscapeExit) {
 			// Grab the current modal tag based on the modalId attribute in the bootstrapModal tag
-			var modal = $('#' + attrs.modalID);
+			var modal = $('#' + attrs.modalId);
 
 			if (hasBackground) {
-				// Check if there is a modalBackground tag and add it if it doesn't exist
-				if (!document.getElementById('modalBackground')) {
-					$('body').append('<div id="modalBackground" class="modalBackground"></div>');
+				// Check if there is a modal-backdrop tag and add it if it doesn't exist
+				if (!document.getElementById('modal-backdrop')) {
+					$('body').append('<div id="modal-backdrop" class="modal-backdrop"></div>');
 				}
 
 				// Show the modal background and bind a click event to the function for closing the modal
-				$('#modalBackground').
+				$('#modal-backdrop').
 					css({display: 'block'}).
 					bind('click', closeModal);
 			}
@@ -57,26 +57,26 @@ directive('bootstrapModal', function($defer) {
 
 			// Display the modal
 			modal.css({display: 'block'});
-		}
+		};
 
 		closeModal = function(event) {
-			// Clean up the modalBackground by removing the click event and hiding it.
-			$('#modalBackground').
+			// Clean up the modal-backdrop by removing the click event and hiding it.
+			$('#modal-backdrop').
 				unbind('click', closeModal).
-				css({diplay: 'none'});
+				css({display: 'none'});
 
 			// Remove the event for closing the body from the body
 			$('body').
 				unbind('keyup', escapeEvent)
 
 			// Hide the modal
-			$('#' + attrs.modalId).css({display: 'hidden'});
-		}
+			$('#' + attrs.modalId).css({display: 'none'});
+		};
 
 		// We need to bind the close and open modal events so outside elements can trigger the modal.
 		// This has to wait until the template has been fully inserted, so just wait a bit of time
 		// before we set them. I'm sure there's a better way of handling this...
-		$defer(function() {
+		$timeout(function() {
 			$('#' + attrs.modalId).
 				bind('modalOpen', openModal).
 				bind('modalClose', closeModal);
@@ -85,27 +85,28 @@ directive('bootstrapModal', function($defer) {
 
 	return {
 		link: link,
-		restric: 'E',
+		replace: true,
+		restrict: 'E',
 		scope: {
-			modalId: 'attribute'
+			modalId: '@' 
 		},
 		template: '<div id="{{modalId}}" class="modal hide"><div ng-transclude></div></div>',
 		transclude: true
 	};
 }).
-// This directive process bootstrapModalOpen attributes. This lets the user bind the opening of a specific modal
+// This directive process bootstrap-modal-open attributes. This lets the user bind the opening of a specific modal
 // as well as specifying how that modal should open.
 directive('bootstrapModalOpen', function() {
 	return {
 		restrict: 'A',
 		link: function(scope, elem, attrs) {
 			// Default to showing the background if the user didn't specify a value for this.
-			var hasBackground = attrs.background === undefined ? true : attrs.background;
+			var hasBackground = (attrs.background === undefined ? true : attrs.background);
 			// Default to allowing an exit on escape if the user didn't provide a value for this.
-			var hasEscapeExit == attrs.escapeExit === undefined ? true : attrs.escapeExit;
+			var hasEscapeExit = (attrs.escapeExit === undefined ? true : attrs.escapeExit);
 
 			$(elem).bind('click', function() {
-				$('#' + attrs.bootstrapModalOpen).trigger('modalOpen', [hasBackground, hasEscapeExit]);
+				$('#' + attrs.bootstrapModalOpen).trigger('modalOpen', hasBackground, hasEscapeExit);
 			});
 		}
 	};
