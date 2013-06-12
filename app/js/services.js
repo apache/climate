bf8b39d860855697808ed655a4adf0dd3cbf0cbd
joyce <joyce@unknown>
2013-06-12 23:15:52 +0000

    Resolves CLIMATE-94 - Move services from app.js to services.js
    git-svn-id: https://svn.apache.org/repos/asf/incubator/climate/trunk/rcmet/src/main/ui@1492467 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 4f6ad89d20f821502991805c4c69cfa0cc542b11

--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,66 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('rcmes').
+	service('selectedDatasetInformation', function() {
+		var datasets = [];
+
+		return {
+			getDatasets: function() {
+				return datasets;
+			},
+			getDatasetCount: function() {
+				return datasets.length;
+			},
+			// TODO: Define the structure of the objects that are added with addDataset.
+			addDataset: function(dataset) {
+				// All datasets need a shouldDisplay attribute that is used when rendering
+				// the overlays on the map!
+				dataset.shouldDisplay = true;
+				// The regrid attribute indicates which dataset should be used for spatial regridding
+				dataset.regrid = false;
+
+				datasets.push(dataset);
+			},
+			removeDataset: function(index) {
+				datasets.splice(index, 1);
+			},
+			clearDatasets: function() {
+				datasets.length = 0;
+			},
+		};
+	}).
+	service('regionSelectParams', function() {
+		var parameters = {
+			"areValid" : true,
+			"latMin"   : "",
+			"latMax"   : "",
+			"lonMin"   : "",
+			"lonMax"   : "",
+			"start"    : "",
+			"end"      : "",
+		};
+
+		return {
+			getParameters: function() {
+				return parameters;
+			},
+		};
+    }).
+	service('evaluationSettings', function() {
+		var settings = {
+			'metrics': [ 
+				{'name': 'bias', 'select': true},
+			],
+			'temporal': {
+				'options': ['daily', 'monthly', 'yearly'],
+				'selected': 'monthly',
+			},
+		};
+
+		return {
+			getSettings: function() {
+				return settings;
+			}
+		};
+	});		
