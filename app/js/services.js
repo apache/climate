e071af6ad053731e4b4d0ef84026fc65d1f374cd
joyce <joyce@unknown>
2013-06-12 23:18:36 +0000

    CLIMATE-99 progress - Moves RegionSelectParams to new file
    git-svn-id: https://svn.apache.org/repos/asf/incubator/climate/trunk/rcmet/src/main/ui@1492484 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 695db8470e6b6056bf2bf3a106dcdaa2dfa2581e

--- app/js/services.js
+++ app/js/services.js
@@ -5,25 +5,7 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('rcmes').
-	service('regionSelectParams', function() {
-		var parameters = {
-			"areValid" : true,
-			"latMin"   : "",
-			"latMax"   : "",
-			"lonMin"   : "",
-			"lonMax"   : "",
-			"start"    : "",
-			"end"      : "",
-		};
-
-		return {
-			getParameters: function() {
-				return parameters;
-			},
-		};
-    }).
-	service('evaluationSettings', function() {
+angular.module('rcmes').service('evaluationSettings', function() {
 		var settings = {
 			'metrics': [ 
 				{'name': 'bias', 'select': true},
