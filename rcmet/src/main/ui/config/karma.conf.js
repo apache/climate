670b7abfd5edeea6359f4ff3f66adc0b91648e06
joyce <joyce@unknown>
2013-06-18 03:16:31 +0000

    Resolve CLIMATE-112 - Unit tests don't run after refactoring
    - The unit tests now run properly. That being said, they're still
      outdated and useless. This will be addressed in a future issue.
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/climate/trunk@1493995 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 52d600a7b7a6ba01ebe97456f63f91d2f4344788

--- rcmet/src/main/ui/config/karma.conf.js
+++ rcmet/src/main/ui/config/karma.conf.js
@@ -3,13 +3,18 @@ basePath = '../';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'app/js/jquery-1.9.1.min.js',
-  'app/js/bootstrap.js',
+  'app/lib/jquery/jquery-1.10.1.js',
+  'app/lib/jquery/jquery-ui/jquery-ui-1.10.3.min.js',
+  'app/lib/bootstrap/bootstrap.js',
   'app/lib/angular/angular.js',
   'app/lib/angular/angular-*.js',
   'test/lib/angular/angular-mocks.js',
-  'app/js/leaflet.js',
-  'app/js/**/*.js',
+  'app/lib/jquery/jquery-ui/datepicker-wrapper/date.js',
+  'app/lib/leaflet/leaflet-0.5.js',
+  'app/js/app.js',
+  'app/js/controllers/*.js',
+  'app/js/directives/*.js',
+  'app/js/services/*.js',
   'test/unit/**/*.js'
 ];
 
