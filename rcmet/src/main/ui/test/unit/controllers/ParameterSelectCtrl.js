d914977dd53f36010161a1eddfdcdd5a30bcaa6a
joyce <joyce@unknown>
2013-06-18 03:16:40 +0000

    CLIMATE-116 progress - Add tests for ParameterSelectCtrl
    - Move ParameterSelectCtrl tests to separate file.
    - Add test to check for spatial/temporal range default value
      initialization and proper dataset service communication. It's possible
      that this test is better suited in the service's test but for now it
      is added here.
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/climate/trunk@1493997 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f3290ee62b46e3af4993676b609ecf8e1f437e3f

---
+++ rcmet/src/main/ui/test/unit/controllers/ParameterSelectCtrl.js
@@ -0,0 +1,37 @@
+'use strict';
+
+describe('OCW Controllers', function() {
+
+	beforeEach(module('ocw.controllers'));
+	beforeEach(module('ocw.services'));
+
+
+	describe('ParameterSelectCtrl', function() {
+		it('initialize spatial and temporal range default values properly', function() {
+			inject(function($rootScope, $controller) {
+				var scope = $rootScope.$new(),
+					ctrl = $controller("ParameterSelectCtrl", {$scope: scope});
+
+				expect(scope.latMin).toBe(-90);
+				expect(scope.latMax).toBe(90);
+				expect(scope.lonMin).toBe(-180);
+				expect(scope.lonMax).toBe(180);
+				expect(scope.start).toBe("1980-01-01 00:00:00");
+				expect(scope.end).toBe("2030-01-01 00:00:00");
+			});
+		});
+
+		it('grab the default set of selected datasets from the service', function() {
+			inject(function($rootScope, $controller) {
+				var scope = $rootScope.$new();
+				var ctrl = $controller("ParameterSelectCtrl", {$scope: scope});
+
+				// We should get an object with no keys since the user hasn't selected any
+				// datasets by default. Object.keys returns an array of all the user defined
+				// keys in the object.
+				expect(typeof scope.datasets).toBe('object');
+				expect(Object.keys(scope.datasets).length).toBe(0);
+			});
+		});
+	});
+});
