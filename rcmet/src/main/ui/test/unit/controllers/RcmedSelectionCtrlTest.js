5b70b9df752f1c28da357688aa3d83adbc484aef
joyce <joyce@unknown>
2013-06-18 18:50:09 +0000

    CLIMATE-123 progress - Add RcmedSelectionCtrl initialization test.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/climate/trunk@1494248 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a8d5637dd9b5dd366d0aeffc505ec7ab3df4ab42

---
+++ rcmet/src/main/ui/test/unit/controllers/RcmedSelectionCtrlTest.js
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http: *www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+**/
+
+'use strict';
+
+describe('OCW Controllers', function() {
+
+	beforeEach(module('ocw.controllers'));
+	beforeEach(module('ocw.services'));
+
+	describe('RcmedSelectionCtrl', function() {
+		it('should automatically query RCMED on initialization', function() {
+			inject(function($rootScope, $controller, $httpBackend) {
+				$rootScope.baseURL = "http://localhost:8082"
+				$httpBackend.expectJSONP($rootScope.baseURL + '/getObsDatasets?callback=JSON_CALLBACK').
+					respond(200, [{longname: 1}, {longname: 2}]);
+
+				var scope = $rootScope.$new();
+				var ctrl = $controller("RcmedSelectionCtrl", {$scope: scope});
+				$httpBackend.flush();
+
+				expect(scope.availableObs.length).toBe(3);
+				expect(scope.availableObs[0]).toEqual({longname: "Please select an option"});
+				expect(scope.availableObs[1]).toEqual({longname: 1});
+			});
+		});
+	});
+});
