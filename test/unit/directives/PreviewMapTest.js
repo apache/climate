1c23671d300fc5e4a47cac716f447bb86924318e
joyce <joyce@unknown>
2013-07-09 21:21:09 +0000

    CLIMATE-184 progress - Initial testing for PreviewMap.js
    git-svn-id: https://svn.apache.org/repos/asf/incubator/climate/trunk/rcmet/src/main/ui@1501563 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6870d55c1dc46aecb0cd2f3cb4eeeb08768e4982

---
+++ test/unit/directives/PreviewMapTest.js
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+**/
+
+'use strict';
+
+describe('directives', function() {
+	beforeEach(module('ocw'));
+
+	describe('preview-map directive', function() {
+		it('should set the proper class', function() {
+			inject(function($compile, $rootScope) {
+				$rootScope.dataset = {latlonVals: {latMax: 90, lonMax: 90, latMin: -90, lonMin: -90}, name: "TRMM"};
+				$rootScope.$digest();
+
+				var element = $compile('<div preview-map="dataset"></div>')($rootScope);
+
+				expect(element.hasClass("preview-map")).toBeTruthy();
+			});
+		});
+	});
+});
