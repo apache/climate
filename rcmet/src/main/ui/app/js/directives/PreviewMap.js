0709d4dd59c5b7296d51450f5bb28fd3b272f2aa
joyce <joyce@unknown>
2013-07-09 21:21:02 +0000

    CLIMATE-184 progress - Draw region overlay on preview map
    - Add drawing of region area as overlay on the preview map. This works
      well for small datasets. On the large datasets this just ends up
      showing a giant colored blob that doesn't necessarily help the user
      understand the extent of the dataset. I think for global datasets it
      would be better to just display an image of a globe (or something
      similar). If the dataset is just really large then we'll just have to
      keep "trying our best" to display the map in a reasonable manner.
      Unfortunately making the map larger doesn't seem to be an option at
      the moment (as it looks horrible).
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/climate/trunk@1501560 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 577c1bdc6ea87283ed6eb31544772dadd745e491

--- rcmet/src/main/ui/app/js/directives/PreviewMap.js
+++ rcmet/src/main/ui/app/js/directives/PreviewMap.js
@@ -16,9 +16,27 @@ App.Directives.directive('previewMap', function($rootScope) {
 			//create a CloudMade tile layer and add it to the map
 			L.tileLayer('http://{s}.tile.cloudmade.com/57cbb6ca8cac418dbb1a402586df4528/997/256/{z}/{x}/{y}.png', {}).addTo(map);
 
-			var overlapBounds = [[scope.dataset.latlonVals.latMax, scope.dataset.latlonVals.lonMin], 
+			// Zoom the map to the dataset bound regions (or at least try our best to do so)
+			var datasetBounds = [[scope.dataset.latlonVals.latMax, scope.dataset.latlonVals.lonMin], 
 								 [scope.dataset.latlonVals.latMin, scope.dataset.latlonVals.lonMax]];
-			map.fitBounds(overlapBounds, {});
+			map.fitBounds(datasetBounds, {});
+
+			// Draw a colored overlay on the region of the map
+			var maplatlon = scope.dataset.latlonVals;
+			var bounds = [[maplatlon.latMax, maplatlon.lonMin], [maplatlon.latMin, maplatlon.lonMax]];
+
+			var polygon = L.rectangle(bounds,{
+				stroke: false,
+				fillColor: $rootScope.fillColors[i],
+				fillOpacity: 0.6
+			});
+
+			// Add layer to Group
+			var rectangleGroup = L.layerGroup();
+			rectangleGroup.addLayer(polygon);
+
+			// Add the overlay to the map
+			rectangleGroup.addTo(map);
 		}
 	};
 });
