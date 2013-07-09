5de7b681f9804c4c16fe7f704e5ad0370627298f
joyce <joyce@unknown>
2013-07-09 21:21:04 +0000

    CLIMATE-184 progress - Add $observe on template id
    - Any attribute that contains {{}} interpolation will be set to null in
      the attrs parameter during the link function since the first $digest
      since the compilation has yet to run to evaluate it! We can't run a
      $digest in the middle of compilation, so using an $observe (or $watch)
      is the best way to get the values. Before we were basically just lucky
      that it wasn't breaking when we ran it.
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/climate/trunk/rcmet/src/main/ui@1501561 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: e6f268af6c92d0f7229d38cfd368d63d7e656ba7

--- app/js/directives/PreviewMap.js
+++ app/js/directives/PreviewMap.js
@@ -4,39 +4,47 @@ App.Directives.directive('previewMap', function($rootScope) {
 		replace: true,
 		scope: {dataset: '=previewMap'},
 		template: '<div id="{{dataset.name}}" class="preview-map"></div>',
+		replace: true,
 		link: function(scope, element, attrs) {
-			var map = L.map(attrs.id, {
-				//center: [40, 0],
-				zoom: 0,
-				scrollWheelZoom: false,
-				zoomControl: false,
-				attributionControl: false,
+
+			// Any attribute that contains {{}} interpolation will be set to null in the attrs
+			// parameter during the link function since the first $digest since the compilation
+			// has yet to run to evaluate it! We can't run a $digest in the middle of compilation,
+			// so using an $observe (or $watch) is the best way to get the values.
+			attrs.$observe('id', function(newId) {
+				//var map = L.map(scope.dataset.name, {
+				var map = L.map(attrs.id, {
+					zoom: 0,
+					scrollWheelZoom: false,
+					zoomControl: false,
+					attributionControl: false,
+				});
+
+				//create a CloudMade tile layer and add it to the map
+				L.tileLayer('http://{s}.tile.cloudmade.com/57cbb6ca8cac418dbb1a402586df4528/997/256/{z}/{x}/{y}.png', {}).addTo(map);
+
+				// Zoom the map to the dataset bound regions (or at least try our best to do so)
+				var datasetBounds = [[scope.dataset.latlonVals.latMax, scope.dataset.latlonVals.lonMin], 
+									 [scope.dataset.latlonVals.latMin, scope.dataset.latlonVals.lonMax]];
+				map.fitBounds(datasetBounds, {});
+
+				// Draw a colored overlay on the region of the map
+				var maplatlon = scope.dataset.latlonVals;
+				var bounds = [[maplatlon.latMax, maplatlon.lonMin], [maplatlon.latMin, maplatlon.lonMax]];
+
+				var polygon = L.rectangle(bounds,{
+					stroke: false,
+					fillColor: $rootScope.fillColors[i],
+					fillOpacity: 0.6
+				});
+
+				// Add layer to Group
+				var rectangleGroup = L.layerGroup();
+				rectangleGroup.addLayer(polygon);
+
+				// Add the overlay to the map
+				rectangleGroup.addTo(map);
 			});
-
-			//create a CloudMade tile layer and add it to the map
-			L.tileLayer('http://{s}.tile.cloudmade.com/57cbb6ca8cac418dbb1a402586df4528/997/256/{z}/{x}/{y}.png', {}).addTo(map);
-
-			// Zoom the map to the dataset bound regions (or at least try our best to do so)
-			var datasetBounds = [[scope.dataset.latlonVals.latMax, scope.dataset.latlonVals.lonMin], 
-								 [scope.dataset.latlonVals.latMin, scope.dataset.latlonVals.lonMax]];
-			map.fitBounds(datasetBounds, {});
-
-			// Draw a colored overlay on the region of the map
-			var maplatlon = scope.dataset.latlonVals;
-			var bounds = [[maplatlon.latMax, maplatlon.lonMin], [maplatlon.latMin, maplatlon.lonMax]];
-
-			var polygon = L.rectangle(bounds,{
-				stroke: false,
-				fillColor: $rootScope.fillColors[i],
-				fillOpacity: 0.6
-			});
-
-			// Add layer to Group
-			var rectangleGroup = L.layerGroup();
-			rectangleGroup.addLayer(polygon);
-
-			// Add the overlay to the map
-			rectangleGroup.addTo(map);
 		}
 	};
 });
