b3e9affc0f7ac91b5fdea7ffc36c586f28024923
skhudiky <skhudiky@unknown>
2013-08-12 19:28:49 +0000

    CLIMATE-224 Update the OCW UI with the AngularUI UI-Router
    -use app.js to set up states in the module config
    -add angular-ui libraries
    -add separate main page and results page
    -update app index.html to reflect all the changes necessary to allow UI-Router to function properly 
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/climate/trunk/rcmet/src/main/ui@1513230 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b93ec4166668f8fbda1919732da57cba32b812bc

--- app/js/app.js
+++ app/js/app.js
@@ -28,17 +28,64 @@ App.Directives = angular.module('ocw.directives', []);
 App.Controllers = angular.module('ocw.controllers', []);
 App.Filters = angular.module('ocw.filters', []);
 
-angular.module('ocw', ['ocw.services', 'ocw.directives', 'ocw.controllers', 'ocw.filters', 'ui.date', 'ui.bootstrap']).
-	config(['$routeProvider', function($routeProvider) {
-		$routeProvider.
-			when('/obs', {templateUrl: 'partials/selectObservation.html', controller: 'ObservationSelectCtrl'}).
-			when('/rcmed', {templateUrl: 'partials/selectRcmed.html', controller: 'RcmedSelectionCtrl'}).
-			otherwise({redirectTo: '/obs'});
-	}]).
-	run(function($rootScope) {
-		$rootScope.evalResults = ""; 
-		$rootScope.fillColors = ['#ff0000', '#00c90d', '#cd0074', '#f3fd00'];
-		$rootScope.surroundColors = ['#a60000', '#008209', '#8f004b', '#93a400']
-		$rootScope.baseURL = 'http://localhost:8082';
-	});
+angular.module('ocw', ['ocw.services', 'ocw.directives', 'ocw.controllers', 'ocw.filters', 'ui.date', 'ui.bootstrap', 'ui.compat', 'ui.state'])
+.config(
+    [        '$stateProvider', '$routeProvider', '$urlRouterProvider',
+    function ($stateProvider,   $routeProvider,   $urlRouterProvider) {
+      $urlRouterProvider
+        .when('/r?id', '/results/:id')
+        .otherwise('/');
+
+      $routeProvider
+        .when('/evaluation/:id', {
+          redirectTo: '/results/:id',
+        })
+        .when('/', {
+          redirectTo: '/evaluate',
+        });
+
+      $stateProvider
+        .state('main',{
+          url: '/evaluate',
+          templateUrl: 'partials/main.html',
+        })
+        .state('results', {
+          url: '/results',
+          abstract: true,
+          templateUrl: 'partials/results.html',
+          controller: 'ResultCtrl'
+        })
+        .state('results.list', {
+          // parent: 'results',
+          url: '',
+          templateUrl: 'partials/results.list.html',
+        })
+        .state('results.detail', {
+          // parent: 'results',
+          url: '/{resultId}',
+          views: {
+            '': {
+              templateUrl: 'partials/results.detail.html',
+              controller: 'ResultDetailCtrl'
+            },
+            'menu': {
+              templateProvider:
+                [ '$stateParams',
+                function ($stateParams){
+                  return '<hr><small class="muted">result ID: ' + $stateParams.resultId + '</small>';
+                }],
+            },
+          },
+        });
+    }])
+.run(
+    [        '$rootScope', '$state', '$stateParams',
+    function ($rootScope,   $state,   $stateParams) {
+      $rootScope.$state = $state;
+      $rootScope.$stateParams = $stateParams;
+      $rootScope.evalResults = ""; 
+      $rootScope.fillColors = ['#ff0000', '#00c90d', '#cd0074', '#f3fd00'];
+      $rootScope.surroundColors = ['#a60000', '#008209', '#8f004b', '#93a400']
+      $rootScope.baseURL = 'http://localhost:8082';
+}]);
 
