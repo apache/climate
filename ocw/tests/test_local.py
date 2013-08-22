3ddfd11370ec877147fa0012280c5faa5e39820f
boustani <boustani@unknown>
2013-08-22 22:05:48 +0000

    CLIMATE-268: Cam, me and Mike improved local.py to guard the 4D array of values and convert that to 3D array, and also it checks for dimensions of lat, lon and time to make sure they are 1D array. The test_local.py has been updated and passes all tests
    git-svn-id: https://svn.apache.org/repos/asf/incubator/climate/trunk@1516637 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6401acbc0a8252e45bde2e4ebae164a6f59c04dd

--- ocw/tests/test_local.py
+++ ocw/tests/test_local.py
@@ -26,11 +26,84 @@ import ocw.data_source.local as local
 
 class test_load_file(unittest.TestCase):
 
-
     def setUp(self):
+        #Read netCDF file
+        self.file_path = create_netcdf_object()
+        self.netCDF_file = netCDF4.Dataset(self.file_path, 'r')
+        self.latitudes = self.netCDF_file.variables['latitude'][:]
+        self.longitudes = self.netCDF_file.variables['longitude'][:]
+        self.values = self.netCDF_file.variables['value'][:]
+        self.variable_name_list = ['latitude', 'longitude', 'time', 'level', 'value']
+        self.possible_value_name = ['latitude', 'longitude', 'time', 'level']
+        
+    def tearDown(self):
+        os.remove(self.file_path)
+    
+
+    def test_function_load_file_lats(self):
+        '''To test load_file function for latitudes'''
+        self.assertItemsEqual(local.load_file(self.file_path, "value").lats, self.latitudes)
+
+
+    def test_function_load_file_lons(self):
+        '''To test load_file function for longitudes'''
+        self.assertItemsEqual(local.load_file(self.file_path, "value").lons, self.longitudes)
+
+
+    def test_function_load_file_times(self):
+        '''To test load_file function for times'''
+        newTimes = datetime.datetime(2001,01,01), datetime.datetime(2001,02,01), datetime.datetime(2001,03,01)
+        self.assertItemsEqual(local.load_file(self.file_path, "value").times, newTimes)
+
+
+    def test_function_load_file_values(self):
+        '''To test load_file function for values'''
+        new_values = self.values[0,:,:,:]
+        self.assertTrue(numpy.allclose(local.load_file(self.file_path, "value").values, new_values))
+
+class test_get_netcdf_variable_names(unittest.TestCase):
+    def setUp(self):
+        self.netcdf_path = os.path.abspath("../../examples/AFRICA_KNMI-RACMO2.2b_CTL_ERAINT_MM_50km_1989-2008_tasmax.nc")
+        self.invalid_netcdf_path = create_invalid_dimensions_netcdf_object()
+        self.netcdf = netCDF4.Dataset(self.netcdf_path, mode='r')
+
+    def tearDown(self):
+        os.remove(self.invalid_netcdf_path)
+
+    def test_valid_latitude(self):
+        self.lat = local._get_netcdf_variable_name(
+                                        local.LAT_NAMES, 
+                                        self.netcdf, 
+                                        "tasmax")
+        self.assertEquals(self.lat, "rlat")
+
+    def test_invalid_dimension_latitude(self):
+        self.netcdf = netCDF4.Dataset(self.invalid_netcdf_path, mode='r')
+        self.lat = local._get_netcdf_variable_name(
+                                        local.LAT_NAMES,
+                                        self.netcdf,
+                                        "value")
+        self.assertEquals(self.lat, "latitude")
+
+    def test_dimension_variable_name_mismatch(self):
+        self.netcdf = netCDF4.Dataset(self.invalid_netcdf_path, mode='r')
+        self.lat = local._get_netcdf_variable_name(
+                                        ["lat_dim"] + local.LAT_NAMES,
+                                        self.netcdf,
+                                        "value")
+        self.assertEquals(self.lat, "latitude")
+
+    def test_no_match_latitude(self):
+        with self.assertRaises(ValueError):
+            self.lat = local._get_netcdf_variable_name(
+                                            ['notAVarName'],
+                                            self.netcdf, 
+                                            "tasmax")
+
+def create_netcdf_object():
         #To create the temporary netCDF file
-        self.file_path = os.getcwd() + '/temporaryNetcdf.nc'
-        netCDF_file = netCDF4.Dataset(self.file_path, 'w',  format='NETCDF4')
+        file_path = '/tmp/temporaryNetcdf.nc'
+        netCDF_file = netCDF4.Dataset(file_path, 'w',  format='NETCDF4')
         #To create dimensions
         netCDF_file.createDimension('lat_dim', 5)
         netCDF_file.createDimension('lon_dim', 5)
@@ -43,57 +116,62 @@ class test_load_file(unittest.TestCase):
         levels = netCDF_file.createVariable('level', 'd', ('level_dim',))
         values = netCDF_file.createVariable('value', 'd', ('level_dim', 'time_dim', 'lat_dim', 'lon_dim'))
         #To latitudes and longitudes for five values
-        self.latitudes = range(0,5)
-        self.longitudes = range(200,205)
+        latitudes = range(0,5)
+        longitudes = range(200,205)
         #Three months of data
-        self.times = range(3)
         #Two levels
-        self.levels = [100, 200]
+        levels = [100, 200]
         #Create 150 values
-        self.values = numpy.array([i for i in range(150)])
+        values = numpy.array([i for i in range(150)])
         #Reshape values to 4D array (level, time, lats, lons)
-        self.values = self.values.reshape(len(self.levels), len(self.times),len(self.latitudes),len(self.longitudes))
+        values = values.reshape(len(levels), len(times),len(latitudes),len(longitudes))
         #Ingest values to netCDF file
-        latitudes[:] = self.latitudes
-        longitudes[:] = self.longitudes
-        times[:] = self.times
-        levels[:] = self.levels
-        values[:] = self.values
-        #Assigne time info to time variable
+        latitudes[:] = latitudes
+        longitudes[:] = longitudes
+        times[:] = numpy.array(range(3))
+        levels[:] = levels
+        values[:] = values
+        #Assign time info to time variable
         netCDF_file.variables['time'].units = 'months since 2001-01-01 00:00:00' 
         netCDF_file.close()
-        #Read netCDF file
-        self.netCDF_file = netCDF4.Dataset(self.file_path, 'r')
-        self.user_value_variable_name = 'value'
-        self.variable_name_list = ['latitude', 'longitude', 'time', 'level', 'value']
-        self.possible_value_name = ['latitude', 'longitude', 'time', 'level']
+        return file_path
 
-
-    def tearDown(self):
-        '''To remove the temporary netCDF file'''
-        os.remove(self.file_path)
-
-
-    def test_function_load_file_lats(self):
-        '''To test load_file function for latitudes'''
-        self.assertItemsEqual(local.load_file(self.file_path, None).lats, self.latitudes)
-
-
-    def test_function_load_file_lons(self):
-        '''To test load_file function for longitudes'''
-        self.assertItemsEqual(local.load_file(self.file_path, None).lons, self.longitudes)
-
-
-    def test_function_load_file_times(self):
-        '''To test load_file function for times'''
-        newTimes = datetime.datetime(2001,01,01), datetime.datetime(2001,02,01), datetime.datetime(2001,03,01)
-        self.assertItemsEqual(local.load_file(self.file_path, None).times, newTimes)
-
-
-    def test_function_load_file_values(self):
-        '''To test load_file function for values'''
-        new_values = self.values[0,:,:,:]
-        self.assertTrue(numpy.allclose(local.load_file(self.file_path, None).values, new_values))
+def create_invalid_dimensions_netcdf_object():
+        #To create the temporary netCDF file
+        file_path = '/tmp/temporaryNetcdf.nc'
+        netCDF_file = netCDF4.Dataset(file_path, 'w', format='NETCDF4')
+        #To create dimensions
+        netCDF_file.createDimension('lat_dim', 5)
+        netCDF_file.createDimension('lon_dim', 5)
+        netCDF_file.createDimension('time_dim', 3)
+        netCDF_file.createDimension('level_dim', 2)
+        #To create variables
+        latitudes = netCDF_file.createVariable('latitude', 'd', ('lat_dim',))
+        longitudes = netCDF_file.createVariable('longitude', 'd', ('lon_dim',))
+        times = netCDF_file.createVariable('time', 'd', ('time_dim',))
+        levels = netCDF_file.createVariable('level', 'd', ('level_dim',))
+        values = netCDF_file.createVariable('value', 'd', ('level_dim', 'time_dim', 'lat_dim', 'lon_dim'))
+        #To latitudes and longitudes for five values
+        latitudes = range(0,5)
+        longitudes = range(200,205)
+        #Three months of data
+        times = range(3)
+        #Two levels
+        levels = [100, 200]
+        #Create 150 values
+        values = numpy.array([i for i in range(150)])
+        #Reshape values to 4D array (level, time, lats, lons)
+        values = values.reshape(len(levels), len(times),len(latitudes),len(longitudes))
+        #Ingest values to netCDF file
+        latitudes[:] = latitudes
+        longitudes[:] = longitudes
+        times[:] = times
+        levels[:] = levels
+        values[:] = values
+        #Assign time info to time variable
+        netCDF_file.variables['time'].units = 'months since 2001-01-01 00:00:00' 
+        netCDF_file.close()
+        return file_path
 
 
 if __name__ == '__main__':
