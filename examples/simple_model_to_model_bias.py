491ce3d2a45ee28a6031025233ff495489c2aeb0
Cameron Eugene Goodale <goodale@apache.org>
2013-08-28 13:52:52 +0000

    CLIMATE-263: Changed the name of the python file to use underscores instead of dashes in the name.
    I also cleaned up some of the print statements in the file as well.
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/climate/trunk@1518205 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: dc0e5099bb507bbc33ff6043d6d120728e56a654

--- examples/simple_model_to_model_bias.py
+++ examples/simple_model_to_model_bias.py
@@ -1,19 +1,13 @@
 import datetime
-
 import ocw.data_source.local as local
 import ocw.dataset_processor as dsp
 
 # Two Local Model Files 
 FILE_1 = "AFRICA_KNMI-RACMO2.2b_CTL_ERAINT_MM_50km_1989-2008_tasmax.nc"
 FILE_2 = "AFRICA_UC-WRF311_CTL_ERAINT_MM_50km-rg_1989-2008_tasmax.nc"
-
 # Load the Files into OCW Objects Using the local from ocw.data_source
 dataset_1 = local.load_file(FILE_1, "tasmax")
 dataset_2 = local.load_file(FILE_2, "tasmax")
-
-print dataset_1.values.shape
-print dataset_2.values.shape
-
 # Do Temporal Rebinning to an Annual Time Step using dataset_processor
 dataset_1 = dsp.temporal_rebin(dataset_1, datetime.timedelta(days=365))
 dataset_2 = dsp.temporal_rebin(dataset_2, datetime.timedelta(days=365))
