83c10c6c1e11c81e3bb4f19a82ec62059de3a69e
joyce <joyce@unknown>
2013-09-16 16:43:49 +0000

    CLIMATE-300 - Fix setup.py packages
    - Add data_source and tests packages to setup.py so everything is
      included when built.
    - Fix mixed " and ' quotes.
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/climate/trunk@1523728 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6381235ada17f7bea2f1894e928471e93bfd5cc7

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name="Apache Open Climate Workbench",
-    version="0.3-incubating-dev",
-    url="http://climate.incubator.apache.org/index.html",
-    author="Apache Open Climate Workbench",
-    author_email="dev@climate.incubator.apache.org",
-    packages=['ocw',],
-    license="Apache License, Version 2.0",
-    long_description="The Apache Open Climate Workbench provides tools for the evaluation and analysis of climate models."
+    name='Apache Open Climate Workbench',
+    version='0.3-incubating-dev',
+    url='http://climate.incubator.apache.org/index.html',
+    author='Apache Open Climate Workbench',
+    author_email='dev@climate.incubator.apache.org',
+    packages=['ocw', 'ocw.data_source', 'ocw.tests'],
+    license='Apache License, Version 2.0',
+    long_description='The Apache Open Climate Workbench provides tools for the evaluation and analysis of climate models.'
 )
