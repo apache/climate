9d2373df2354f8c477ff337820824d08070284df
joyce <joyce@unknown>
2013-09-16 17:15:11 +0000

    Typo fix in setup.py
    git-svn-id: https://svn.apache.org/repos/asf/incubator/climate/trunk@1523731 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 20f4cfaa0a6c5ca426c79f8d2a21e45dae3f7c86

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     url='http://climate.incubator.apache.org/index.html',
     author='Apache Open Climate Workbench',
     author_email='dev@climate.incubator.apache.org',
-    packages=['ocw', 'ocw.data_sourcets ', 'ocw.tests'],
+    packages=['ocw', 'ocw.data_source', 'ocw.tests'],
     license='Apache License, Version 2.0',
     long_description='The Apache Open Climate Workbench provides tools for the evaluation and analysis of climate models.'
 )
