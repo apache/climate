28ae61a294c373e419e489032cfa849e5afa9812
joyce <joyce@unknown>
2013-09-16 16:43:50 +0000

    CLIMATE-300 - Add Apache License header
    git-svn-id: https://svn.apache.org/repos/asf/incubator/climate/trunk@1523729 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a33a9873db0904043fc8a55bf2209ddc7e6f0ba0

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 from distutils.core import setup
 
 setup(
@@ -6,7 +23,7 @@ setup(
     url='http://climate.incubator.apache.org/index.html',
     author='Apache Open Climate Workbench',
     author_email='dev@climate.incubator.apache.org',
-    packages=['ocw', 'ocw.data_source', 'ocw.tests'],
+    packages=['ocw', 'ocw.data_sourcets ', 'ocw.tests'],
     license='Apache License, Version 2.0',
     long_description='The Apache Open Climate Workbench provides tools for the evaluation and analysis of climate models.'
 )
