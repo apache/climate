c5cb3cb007b2fa41a07ed32cd8c3dae4b115ebd9
joyce <joyce@unknown>
2013-10-02 23:48:26 +0000

    CLIMATE-309 - Init unit tests
    git-svn-id: https://svn.apache.org/repos/asf/incubator/climate/trunk@1528687 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c3726b34348ff2e24e0d8c2829c5cfbc69fd74fd

---
+++ ocw/tests/test_dap.py
@@ -0,0 +1,30 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import unittest
+import ocw.data_source.dap as dap
+from ocw.dataset import Dataset
+
+class test_dap(unittest.TestCase):
+    def setUp(self):
+        self.dataset = dap.load('http://test.opendap.org/dap/data/nc/sst.mnmean.nc.gz', 'sst')
+
+    def test_dataset_is_returned(self):
+        self.assertTrue(isinstance(self.dataset, Dataset))
+
+if __name__ == '__main__':
+    unittest.main()
