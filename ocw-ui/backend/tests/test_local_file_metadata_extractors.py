e34d93c381f698c5bae8f0ab3469cfc6bb753abc
joyce <joyce@unknown>
2014-01-28 22:19:33 +0000

    CLIMATE-326 - Add successful jsonp request test
    git-svn-id: https://svn.apache.org/repos/asf/incubator/climate/trunk@1562267 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 7155fe2efa281d57f59a5eccc3fea625557da6b4

--- ocw-ui/backend/tests/test_local_file_metadata_extractors.py
+++ ocw-ui/backend/tests/test_local_file_metadata_extractors.py
@@ -1,8 +1,7 @@
 import os
+from ast import literal_eval
 import unittest
 from webtest import TestApp
-
-import urllib
 
 from ..run_webservices import app
 
@@ -25,6 +24,32 @@ class TestLatLonExtraction(unittest.TestCase):
         response = test_app.get('/lfme/list_latlon/' + file_location)
 
         self.assertDictEqual(expected_return, response.json)
+
+    def test_successful_latlon_extract_jsonp(self):
+        expected_return = {
+            'lon_min': -24.639999389648438,
+            'lat_max': -45.759998321533203,
+            'success': True,
+            'lon_name': 'lon',
+            'lat_name': 'lat',
+            'lon_max': 60.279998779296875,
+            'lat_min': -45.759998321533203
+        }
+
+        file_location = os.path.abspath('tests/example_data/lat_lon_time.nc')
+
+        response = test_app.get('/lfme/list_latlon/' + file_location + '?callback=test_callback')
+
+        json = response.text
+
+        # Strip out the callback functino and the json string from the response
+        # and check for proper content.
+        callback = json[:json.index('(')]
+        json = json[json.index('(') + 1 : json.rindex(')')]
+        json = literal_eval(json)
+
+        self.assertDictEqual(expected_return, json)
+        self.assertEqual(callback, "test_callback")
 
     def test_failure_latlon_extract(self):
         expected_return = {
