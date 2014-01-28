aed22dd12e0703180000005584b060362fe902d8
joyce <joyce@unknown>
2014-01-28 22:19:18 +0000

    CLIMATE-326 - Add successful lfme/list_latlon test
    git-svn-id: https://svn.apache.org/repos/asf/incubator/climate/trunk@1562264 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 30a6492c71f97aff6b898c9d709cdedf9ed7fa4f

--- ocw-ui/backend/tests/test_local_file_metadata_extractors.py
+++ ocw-ui/backend/tests/test_local_file_metadata_extractors.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from webtest import TestApp
 
@@ -9,7 +10,21 @@ test_app = TestApp(app)
 
 class TestLatLonExtraction(unittest.TestCase):
     def test_successful_latlon_extract(self):
-        pass
+        expected_return = {
+            "success": True,
+            "lat_name": "lat",
+            "lon_name": "lon",
+            "lat_min": -45.759998321533203,
+            "lat_max": -45.759998321533203,
+            "lon_min": -24.639999389648438,
+            "lon_max": 60.279998779296875
+        }
+
+        file_location = os.path.abspath('tests/example_data/lat_lon_time.nc')
+
+        response = test_app.get('/lfme/list_latlon/' + file_location)
+
+        self.assertDictEqual(expected_return, response.json)
 
     def test_failure_latlon_extract(self):
         pass
