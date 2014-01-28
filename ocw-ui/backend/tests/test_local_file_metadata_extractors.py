144ad90b5427bcb781f2669edd53d9beb665fc30
joyce <joyce@unknown>
2014-01-28 22:19:38 +0000

    Add failure lfme/lis_latlon jsonp test
    git-svn-id: https://svn.apache.org/repos/asf/incubator/climate/trunk@1562268 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 98764bc3057e73f95eeac41133330e0be1e4f3f6

--- ocw-ui/backend/tests/test_local_file_metadata_extractors.py
+++ ocw-ui/backend/tests/test_local_file_metadata_extractors.py
@@ -63,5 +63,25 @@ class TestLatLonExtraction(unittest.TestCase):
 
         self.assertDictEqual(expected_return, response.json)
 
+    def test_failure_latlon_extract_jsonp(self):
+        expected_return = {
+            "success": False,
+            "variables": ["invalid_lon", "invalid_time", "invalid_lat"]
+        }
+
+        file_location = os.path.abspath('tests/example_data/lat_lon_time_invalid.nc')
+
+        response = test_app.get('/lfme/list_latlon/' + file_location + '?callback=test_callback')
+        json = response.text
+
+        # Strip out the callback functino and the json string from the response
+        # and check for proper content.
+        callback = json[:json.index('(')]
+        json = json[json.index('(') + 1 : json.rindex(')')]
+        json = literal_eval(json)
+
+        self.assertDictEqual(expected_return, json)
+        self.assertEqual(callback, "test_callback")
+
 if __name__ == '__main__':
     unittest.main()
