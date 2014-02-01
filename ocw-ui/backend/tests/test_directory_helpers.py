dbfa14401c0b50eb1a3cac413652cb975ee9d41f
joyce <joyce@unknown>
2014-02-01 23:57:10 +0000

    Add valid directory cleaner helper test
    git-svn-id: https://svn.apache.org/repos/asf/incubator/climate/trunk@1563517 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a23ba7556854cb30faaa0dfc19fdbcc6cb67382c

---
+++ ocw-ui/backend/tests/test_directory_helpers.py
@@ -0,0 +1,19 @@
+import os
+import unittest
+from webtest import TestApp
+
+from ..run_webservices import app
+from ..directory_helpers import _get_clean_directory_path
+
+test_app = TestApp(app)
+
+class TestDirectoryPathCleaner(unittest.TestCase):
+    PATH_LEADER = '/tmp/foo'
+    VALID_CLEAN_DIR = '/tmp/foo/bar'
+
+    if not os.path.exists(PATH_LEADER): os.mkdir(PATH_LEADER)
+    if not os.path.exists(VALID_CLEAN_DIR): os.mkdir(VALID_CLEAN_DIR)
+
+    def test_valid_directory_path(self):
+        clean_path = _get_clean_directory_path(self.PATH_LEADER, '/bar')
+        self.assertEquals(clean_path, self.VALID_CLEAN_DIR)
