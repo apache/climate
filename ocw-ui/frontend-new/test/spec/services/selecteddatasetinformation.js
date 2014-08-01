c1691570ef1de65ae375479de5c9454043d8414a
Michael Joyce <joyce@apache.org>
2014-08-01 13:53:11 -0700

    Transition selectdatasetinformation service tests

--- ocw-ui/frontend-new/test/spec/services/selecteddatasetinformation.js
+++ ocw-ui/frontend-new/test/spec/services/selecteddatasetinformation.js
@@ -30,8 +30,63 @@ describe('Service: selectedDatasetInformation', function () {
     selectedDatasetInformation = _selectedDatasetInformation_;
   }));
 
-  it('should do something', function () {
-    expect(!!selectedDatasetInformation).toBe(true);
+  it('should initialize the selectedDatasetInformation service', function() {
+    inject(function(selectedDatasetInformation) {
+      expect(selectedDatasetInformation).not.toEqual(null);
+    });
   });
 
+  it('should provide the getDatasets function', function() {
+    inject(function(selectedDatasetInformation) {
+      expect(selectedDatasetInformation.getDatasets()).not.toEqual(null);
+    });
+  });
+
+  it('should provide the getDatasetCount function', function() {
+    inject(function(selectedDatasetInformation) {
+      expect(selectedDatasetInformation.getDatasetCount()).toEqual(0);
+    });
+  });
+
+  it('should provide the addDataset function', function() {
+    inject(function(selectedDatasetInformation) {
+      selectedDatasetInformation.addDataset({});
+      expect(selectedDatasetInformation.getDatasetCount()).toEqual(1);
+    });
+  });
+
+  it('should set the shouldDisplay attribute when adding a dataset', function() {
+    inject(function(selectedDatasetInformation) {
+      selectedDatasetInformation.addDataset({});
+      expect(selectedDatasetInformation.getDatasets()[0].shouldDisplay).toBe(false);
+    });
+  });
+
+  it('should set the regrid attribute when adding a dataset', function() {
+    inject(function(selectedDatasetInformation) {
+      selectedDatasetInformation.addDataset({});
+      expect(selectedDatasetInformation.getDatasets()[0].regrid).toBe(false);
+    });
+  });
+
+  it('should provide the removeDataset function', function() {
+    inject(function(selectedDatasetInformation) {
+      selectedDatasetInformation.addDataset(1);
+      selectedDatasetInformation.addDataset(2);
+
+      expect(selectedDatasetInformation.getDatasets()[0]).toEqual(1);
+      selectedDatasetInformation.removeDataset(0);
+      expect(selectedDatasetInformation.getDatasets()[0]).toEqual(2);
+    });
+  });
+
+  it('should provide the clearDatasets function', function() {
+    inject(function(selectedDatasetInformation) {
+      selectedDatasetInformation.addDataset({});
+      expect(selectedDatasetInformation.getDatasetCount()).toEqual(1);
+
+      selectedDatasetInformation.clearDatasets();
+      expect(selectedDatasetInformation.getDatasetCount()).toEqual(0);
+    });
+  });
 });
