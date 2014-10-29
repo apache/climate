8334a839625cf07ef3201b25b1820e5e0bfebfec
Lewis John McGibbney <lewis.j.mcgibbney@jpl.nasa.gov>
2014-10-29 14:45:15 -0700

    CLIMATE-539 Get OCW on to PyPI

--- setup.py
+++ setup.py
@@ -15,15 +15,73 @@
 # specific language governing permissions and limitations
 # under the License.
 
-from setuptools import setup
+import os.path
+from setuptools import find_packages, setup
+
+# Package data
+# ------------
+
+_name         = 'ocw'
+_version      = '0.5'
+_description  = 'Apache Open Climate Workbench'
+_url          = 'http://climate.apache.org/'
+_downloadURL  = 'http://pypi.python.org/pypi/ocw/'
+_author       = 'Apache Open Climate Workbench'
+_authorEmail  = 'dev@climate.apache.org'
+_license      = 'Apache License, Version 2.0'
+_namespaces   = []
+_testSuite    = 'ocw.tests'
+_zipSafe      = True
+_keywords     = ['climate analysis', 'workbench', 'rebinning', 'metrics', 'computation', 'visualization']
+_requirements = []
+_classifiers  = [
+    'Environment :: Console',
+    'Environment :: No Input/Output (Daemon)',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Information Technology',
+    'Intended Audience :: Science/Research',
+    'Topic :: Database :: Front-Ends',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+    'Topic :: Scientific/Engineering',
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+# Setup Metadata
+# --------------
+
+def _read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+_header = '*' * len(_name) + '\n' + _name + '\n' + '*' * len(_name)
+_longDescription = '\n\n'.join([
+    _header,
+    _read('README.md')
+])
+open('doc.txt', 'w').write(_longDescription)
 
 setup(
-    name='Apache Open Climate Workbench',
-    version='0.4',
-    url='http://climate.apache.org/index.html',
-    author='Apache Open Climate Workbench',
-    author_email='dev@climate.apache.org',
-    packages=['ocw', 'ocw.data_source', 'ocw.tests', 'ocw.esgf'],
-    license='Apache License, Version 2.0',
-    long_description='The Apache Open Climate Workbench provides tools for the evaluation and analysis of climate models.'
+    author=_author,
+    author_email=_authorEmail,
+    classifiers=_classifiers,
+    description=_description,
+    download_url=_downloadURL,
+    include_package_data=True,
+    install_requires=_requirements,
+    keywords=_keywords,
+    license=_license,
+    long_description=_longDescription,
+    name=_name,
+    namespace_packages=_namespaces,
+    packages=find_packages(),
+    test_suite=_testSuite,
+    url=_url,
+    version=_version,
+    zip_safe=_zipSafe,
 )
