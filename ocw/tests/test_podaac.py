7d53faac39c4a5198b5d3971fda0318865b1c48e
Omkar20895 <omkarreddy2008@gmail.com>
2016-08-11 02:10:42 +0530

    Fixing all the PEP8 violations

--- ocw/tests/test_podaac.py
+++ ocw/tests/test_podaac.py
@@ -17,45 +17,49 @@
 
 
 import ocw.data_source.podaac as podaac
-import unittest, os
+import unittest
+import os
 from ocw.dataset import Dataset
 
+
 class TestPodaacDataSource(unittest.TestCase):
-	@classmethod
-	def setUpClass(cls):
-		cls.datasetId = 'PODAAC-GHCMC-4FM02'
-		cls.datasetShortName = 'CMC0.2deg-CMC-L4-GLOB-v2.0'
-		cls.variable = 'sea_ice_fraction'
-		cls.name = 'PO.DAAC_test_dataset'
-		cls.file_path = os.path.dirname(os.path.abspath(__file__))
-		cls.format = '.nc'
-		cls.dataset = podaac.load_dataset(cls.variable, cls.datasetId, cls.datasetShortName, cls.name)	
 
-	def test_is_dataset(self):
-		self.assertTrue(isinstance(self.dataset, Dataset))
-	
-	def test_dataset_lats(self):
-		self.assertEquals(len(self.dataset.lats), 901)
+    @classmethod
+    def setUpClass(cls):
+        cls.datasetId = 'PODAAC-GHCMC-4FM02'
+        cls.datasetShortName = 'CMC0.2deg-CMC-L4-GLOB-v2.0'
+        cls.variable = 'sea_ice_fraction'
+        cls.name = 'PO.DAAC_test_dataset'
+        cls.file_path = os.path.dirname(os.path.abspath(__file__))
+        cls.format = '.nc'
+        cls.dataset = podaac.load_dataset(
+            cls.variable, cls.datasetId, cls.datasetShortName, cls.name)
 
-	def test_dataset_lons(self):
-		self.assertEquals(len(self.dataset.lons), 1800)
+    def test_is_dataset(self):
+        self.assertTrue(isinstance(self.dataset, Dataset))
 
-	def test_dataset_times(self):
-		self.assertEquals(len(self.dataset.times), 1)
+    def test_dataset_lats(self):
+        self.assertEquals(len(self.dataset.lats), 901)
 
-	def test_dataset_values(self):
-		self.assertEquals(len(self.dataset.values),1)
+    def test_dataset_lons(self):
+        self.assertEquals(len(self.dataset.lons), 1800)
 
-	def test_valid_date_conversion(self):
-        start = dt.datetime(2006, 6, 7, 12)
-        self.assertTrue(start == self.dataset.times[0])
+    def test_dataset_times(self):
+        self.assertEquals(len(self.dataset.times), 1)
 
-	def test_dataset_origin(self):
-		self.assertEquals(self.dataset.origin['source'], 'PO.DAAC')
-		self.assertEquals(self.dataset.origin['url'], 'podaac.jpl.nasa.gov/ws')
+    def test_dataset_values(self):
+        self.assertEquals(len(self.dataset.values), 1)
 
-	def test_custom_name(self):
-		self.assertEquals(self.dataset.name, self.name)
+    def test_valid_date_conversion(self):
+    start = dt.datetime(2006, 6, 7, 12)
+    self.assertTrue(start == self.dataset.times[0])
+
+    def test_dataset_origin(self):
+        self.assertEquals(self.dataset.origin['source'], 'PO.DAAC')
+        self.assertEquals(self.dataset.origin['url'], 'podaac.jpl.nasa.gov/ws')
+
+    def test_custom_name(self):
+        self.assertEquals(self.dataset.name, self.name)
 
 
 if __name__ == '__main__':
