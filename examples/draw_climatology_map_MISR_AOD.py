0981830adbcac53524317ccd62b6f2ced1ecea46
huikyole <huikyole@argo.jpl.nasa.gov>
2016-08-25 08:24:09 -0700

    revised with comments

--- examples/draw_climatology_map_MISR_AOD.py
+++ examples/draw_climatology_map_MISR_AOD.py
@@ -1,3 +1,20 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 import ocw.dataset as ds
 import ocw.data_source.local as local
 import ocw.dataset_processor as dsp
@@ -7,8 +24,9 @@ import numpy as np
 import numpy.ma as ma
 
 
-''' data source: https://dx.doi.org/10.6084/m9.figshare.3753321.v1'''
-dataset = local.load_file('/home/huikyole/climate/examples/AOD_monthly_2000-MAR_2016-FEB_from_MISR_L3_JOINT.nc', 
+''' data source: https://dx.doi.org/10.6084/m9.figshare.3753321.v1
+    AOD_monthly_2000-Mar_2016-FEB_from_MISR_L3_JOINT.nc is publicly available.'''
+dataset = local.load_file('AOD_monthly_2000-MAR_2016-FEB_from_MISR_L3_JOINT.nc', 
                           'nonabsorbing_ave')
 ''' Subset the data for East Asia'''
 Bounds = ds.Bounds(lat_min=20, lat_max=57.7, lon_min=90, lon_max=150)
